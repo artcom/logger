cac4fbb06c09e22c0e54488c8b0146e3587178ee
Mirko Geissler <mirko.geissler@artcom.de>
2020-03-18 16:07:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# BLogger
+
+A [Winston](https://github.com/winstonjs/winston) logger which logs to JSON with ordered keys and error stack support.
+
+# Usage
+
+Install this library via npm:
+```bash
+npm install @artcom/logger
+```
+
+Use as follows:
+```javascript
+const { createLogger } = require("@artcom/logger")
+const logger = createLogger()
+
+logger.info("Hello world!")
+```
+
+Additional options are supported:
+* `transports`: is the array of transports managed by [Winston](https://github.com/winstonjs/winston)
+* `keyOrder` is the array specifying the order of message keys (e.g. ["timestamp", "level", "message"]), additional keys are placed after these keys by insertion order
---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import * as Winston from "winston"
+
+const logErrorProp = Winston.format(info => {
+  if (info.error instanceof Error) {
+    info.error = { ...info.error, message: info.error.message, stack: info.error.stack }
+  }
+  return info
+})
+
+const reorderKeys = Winston.format((info, keyOrder) => {
+  const orderedKeys = [...new Set([...keyOrder, ...Object.keys(info)])]
+
+  // simply delete and re-add keys to ensure order
+  orderedKeys.forEach(key => {
+    const value = info[key]
+    delete info[key]
+    info[key] = value
+  })
+
+  return info
+})
+
+export function createLogger({
+  transports = [new Winston.transports.Console({ level: "debug" })],
+  keyOrder = ["timestamp", "level", "message"]
+} = {}) : Winston.Logger {
+  return Winston.createLogger({
+    format: Winston.format.combine(
+      Winston.format.errors({ stack: true }),
+      logErrorProp(),
+      Winston.format.timestamp(),
+      reorderKeys(keyOrder),
+      Winston.format.json()),
+    transports
+  })
+}
