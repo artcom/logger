636aa50c34b16579a0b3aa6dab03fe67b7711534
Mirko Geissler <mirko.geissler@artcom.de>
2022-01-26 13:04:48 +0100

    Resolve message manually
    This is necessary to:
    * support a custom property order
    * to transform error stacks to arrays for better readability

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,60 @@
 import * as Winston from "winston"
 
+import { LEVEL, MESSAGE } from "triple-beam"
+import { configure } from "safe-stable-stringify"
+
+const stringify = configure({ deterministic: false })
+
 export { Winston }
 
-const logErrorProp = Winston.format(info => {
-  if (info.error instanceof Error) {
-    info.error = { ...info.error, message: info.error.message, stack: info.error.stack }
+function resolveError(obj: any) {
+  return {
+    ...obj,
+    level: obj.level,
+    [LEVEL]: obj[LEVEL as any] || obj.level,
+    message: obj.message,
+    stack: obj.stack.split("\n")
   }
+}
+
+function resolveErrors(obj: any) {
+  if (obj instanceof Error) {
+    return resolveError(obj)
+  }
+
+  if (obj.message instanceof Error) {
+    const { message, stack } = obj.message
+    return {
+      ...obj,
+      ...obj.message,
+      message,
+      stack: stack.split("\n")
+    }
+  }
+
+  if (obj.stack) {
+    return {
+      ...obj,
+      stack: obj.stack.split("\n")
+    }
+  }
+
+  return obj
+}
+
+const toJson = Winston.format((info: any, keyOrderObj) => {
+  info = resolveErrors(info)
+
+  for (const prop in info) {
+    if (info[prop] instanceof Error) {
+      info[prop] = resolveError(info[prop])
+    }
+  }
+
+  info.timestamp = new Date().toISOString()
+  info[MESSAGE as any] = stringify(Object.assign({}, keyOrderObj, info))
   return info
 })
-
-const reorderKeys = Winston.format((info, keyOrderObj) => Object.assign({}, keyOrderObj, info))
 
 export function createLogger({
   transports = [new Winston.transports.Console({ level: "debug" })],
@@ -17,14 +62,5 @@ export function createLogger({
 } = {}) : Winston.Logger {
   const keyOrderObj = keyOrder.reduce((acc: any, key: string) => ({ ...acc, [key]: null }), {})
 
-  return Winston.createLogger({
-    format: Winston.format.combine(
-      Winston.format.errors({ stack: true }),
-      logErrorProp(),
-      Winston.format.timestamp({ format: () => new Date().toLocaleString() }),
-      reorderKeys(keyOrderObj),
-      Winston.format.json()),
-    transports
-  })
+  return Winston.createLogger({ format: toJson(keyOrderObj), transports })
 }
-
